47836ce00f908e2feed01ffae2893d914ceee0e0
XC <3316279993@qq.com>
2026-05-30 00:11:08 +0800

    mapper

--- src/main/java/com/voice/agent/mapper/CalendarEventMapper.java
+++ src/main/java/com/voice/agent/mapper/CalendarEventMapper.java
@@ -4,6 +4,10 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.voice.agent.model.entity.CalendarEventEntity;
 import org.apache.ibatis.annotations.Mapper;
 
+/**
+ * 日程表 calendar_event 的 MyBatis-Plus Mapper。
+ *
+ */
 @Mapper
 public interface CalendarEventMapper extends BaseMapper<CalendarEventEntity> {
 }
